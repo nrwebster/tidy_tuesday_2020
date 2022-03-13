b8e5d9eff9468cb5c6d6ca3a94a47226106d5d57
nrwebster <nrwebster26@gmail.com>
2022-03-13 12:35:17 -0500

    PRG takes rows instead of matrices \^/

--- determinant_4x4.R
+++ determinant_4x4.R
@@ -13,104 +13,117 @@
 
   # load packages
   library(data.table)
-  
+
   # generate data
   # my_dt <- t(matrix(c(4,  3,  2, 2,
   #                     0,  1, -3, 3,
-  #                     0, -1,  3, 3, 
+  #                     0, -1,  3, 3,
   #                     0,  3,  1, 1),
   #                   ncol = 4))
   
-  my_dt <- t(matrix(c(1,  1,  1, -1,
-                      1,  1, -1,  1,
-                      1, -1,  1, 1, 
-                     -1,  1,  1, 1),
-                    ncol = 4))
+  my_dt1 <- data.table(tv = c(4,  3,  2, 2,
+                    0,  1, -3, 3,
+                    0, -1,  3, 3,
+                    0,  3,  1, 1))
   
-  # give dimensions' names
-  colnames(my_dt) <- paste("c", 1:4, sep = "_")
-  rownames(my_dt) <- paste("r", 1:4, sep = "_")
+  my_dt2 <- data.table(tv = c(1,  1,  1, -1,
+                            1,  1, -1,  1,
+                            1, -1,  1,  1, 
+                           -1,  1,  1,  1))
+  
+  # name variables
+  my_dt1[, row := rep(1:4, each  = 4)]
+  my_dt1[, col := rep(1:4, times = 4)]
+  my_dt2[, row := rep(1:4, each  = 4)]
+  my_dt2[, col := rep(1:4, times = 4)]
+  
+  # add student id
+  my_dt1[, student_id := "1"]
+  my_dt2[, student_id := "2"]
+  
+  # cast wide
+  wide_dt1 <- dcast.data.table(my_dt1, student_id ~ paste("tv", row, col, sep = "_"), value.var = "tv")
+  wide_dt2 <- dcast.data.table(my_dt2, student_id ~ paste("tv", row, col, sep = "_"), value.var = "tv")
 
+  # stacked student level
+  stacked_student <- rbind(wide_dt1, wide_dt2)
+  
 
 # function to compute determinant terms
-compute_four_by_four_determinant_terms <- function(cofactor_row, cofactor_column){
+compute_four_by_four_determinant_terms <- function(in_data, cofactor_row, cofactor_column){
   
-  # isolate each term
-  cofactor <- my_dt[cofactor_row, cofactor_column]
+  # isolate each term #note assumes variables names
+  varname_cofactor <- grep(paste(cofactor_row, cofactor_column, sep = "_"), colnames(in_data), value = TRUE)
   
-  # find the 3x3
-  three_by_three <- my_dt[-cofactor_row, -cofactor_column]
+  # find 3x3 element varnames
+  three_by_three_varnames <- grep(paste(paste0("_", cofactor_row, "_"), paste0(cofactor_column, "$"), sep = "|"), colnames(in_data), value = TRUE, invert = TRUE)
   
-  # convert to single row
-  single_vector <- data.table(c(three_by_three))
+  # find the 3x3 by removing the cofactor's shared elements
+  three_by_three <- subset(in_data, select = c(three_by_three_varnames))
   
-  # make colnames
-  single_vector[, col := rep(1:3, each  = 3)]
-  single_vector[, row := rep(1:3, times = 3)]
-  single_vector[, varname := paste0("v_", row, col)]
-  
-  # remove row and cols intermediate names
-  single_vector[, c("row", "col") := NULL]
-  
-  # cast wide to be 1 row
-  wide_dt <- dcast.data.table(single_vector, ... ~ varname, value.var = "V1")[, -1]
+  # rename colnames
+  setnames(three_by_three, three_by_three_varnames, c("student_id", paste0("v_", rep(1:3, each = 3), rep(1:3, times  = 3))))
   
   # forwards diagonal products
-  wide_dt[, fdp_1 := v_11 * v_22 * v_33]
-  wide_dt[, fdp_2 := v_12 * v_23 * v_31]
-  wide_dt[, fdp_3 := v_13 * v_21 * v_32]
+  three_by_three[, fdp_1 := v_11 * v_22 * v_33]
+  three_by_three[, fdp_2 := v_12 * v_23 * v_31]
+  three_by_three[, fdp_3 := v_13 * v_21 * v_32]
   
   # backwards diagonal products
-  wide_dt[, bdp_1 := v_11 * v_23 * v_32]
-  wide_dt[, bdp_2 := v_12 * v_21 * v_33]
-  wide_dt[, bdp_3 := v_13 * v_22 * v_31]
+  three_by_three[, bdp_1 := v_11 * v_23 * v_32]
+  three_by_three[, bdp_2 := v_12 * v_21 * v_33]
+  three_by_three[, bdp_3 := v_13 * v_22 * v_31]
   
   # sum forwards 
-  wide_dt[, forward_sum := fdp_1 + fdp_2 + fdp_3]
+  three_by_three[, forward_sum := fdp_1 + fdp_2 + fdp_3]
   
   # sum backwards 
-  wide_dt[, backward_sum := bdp_1 + bdp_2 + bdp_3]
+  three_by_three[, backward_sum := bdp_1 + bdp_2 + bdp_3]
   
   # subtract
-  wide_dt[, difference := forward_sum - backward_sum]
+  three_by_three[, difference := forward_sum - backward_sum]
   
   # include cofactor to compute 4x4 determinant outside of function
-  wide_dt[, cofactor :=  cofactor]
+  three_by_three[, cofactor := in_data[, get(varname_cofactor)]]
   
   # identify cofactor
-  wide_dt[, cofactor_row    := cofactor_row]
-  wide_dt[, cofactor_column := cofactor_column]
+  three_by_three[, cofactor_row    := cofactor_row]
+  three_by_three[, cofactor_column := cofactor_column]
   
   #note keep the differences for filling in the adjugate
-  return(wide_dt[, .(cofactor_row, cofactor_column, cofactor, difference)])
+  return(three_by_three[, .(student_id, cofactor_row, cofactor_column, cofactor, difference)])
   
 }
 
   # call function for each cofactor
-  determinant_stacked_terms <- rbindlist(lapply(1:nrow(my_dt), function(x) compute_four_by_four_determinant_terms(x, 1)))
-  second_adjugate_terms     <- rbindlist(lapply(1:nrow(my_dt), function(x) compute_four_by_four_determinant_terms(x, 2)))
-  third_adjugate_terms      <- rbindlist(lapply(1:nrow(my_dt), function(x) compute_four_by_four_determinant_terms(x, 3)))
-  fourth_adjugate_terms     <- rbindlist(lapply(1:nrow(my_dt), function(x) compute_four_by_four_determinant_terms(x, 4)))
+  determinant_stacked_terms <- rbindlist(lapply(1:4, function(x) compute_four_by_four_determinant_terms(stacked_student, x, 1)))
+  second_adjugate_terms     <- rbindlist(lapply(1:4, function(x) compute_four_by_four_determinant_terms(stacked_student, x, 2)))
+  third_adjugate_terms      <- rbindlist(lapply(1:4, function(x) compute_four_by_four_determinant_terms(stacked_student, x, 3)))
+  fourth_adjugate_terms     <- rbindlist(lapply(1:4, function(x) compute_four_by_four_determinant_terms(stacked_student, x, 4)))
   
   # stack adjugate terms
   stacked_adjugate <- rbind(determinant_stacked_terms, second_adjugate_terms, third_adjugate_terms, fourth_adjugate_terms)
   
   # compute cofactor products
   determinant_stacked_terms[, determinant_term := cofactor * difference]
-
+  
   # cast wide to get the +- pattern right  
-  wide_determinant <- dcast.data.table(determinant_stacked_terms[, -c("cofactor", "difference")], ... ~ paste0("cf_", cofactor_row, "_", cofactor_column), value.var = "determinant_term")[, -1]
+  wide_determinant <- dcast.data.table(determinant_stacked_terms[, -c("cofactor", "difference")], student_id ~ paste0("cf_", cofactor_row, "_", cofactor_column), value.var = "determinant_term")
   
   # compute determinant
-  determinant <- wide_determinant[, cf_1_1 - cf_2_1 + cf_3_1 - cf_4_1]
+  wide_determinant[, determinant := cf_1_1 - cf_2_1 + cf_3_1 - cf_4_1]
   
   # first term of adjugate depends on even/odd dimsums
   stacked_adjugate[, sign := (-1) ^ (cofactor_row + cofactor_column)]
   
+  # merge determinant onto adjugate
+  setkey(wide_determinant, "student_id")
+  setkey(stacked_adjugate, "student_id")
+  inverse_four_by_four <- merge(wide_determinant, stacked_adjugate, by = "student_id")
+  
   # the inverse is sign times difference times 1 / determinant
-  stacked_adjugate[, inverse_element := 1 / determinant * sign * difference]
-  
-  
-  
-  
-                      
+  inverse_four_by_four[, inverse_element := 1 / determinant * sign * difference]
+
+
+
+
