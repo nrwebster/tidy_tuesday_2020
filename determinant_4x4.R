0070bba6489fbda911dd4889546963711f3ba219
nrwebster <nrwebster26@gmail.com>
2022-03-13 11:33:37 -0500

    PRG adjugate

--- determinant_4x4.R
+++ determinant_4x4.R
@@ -5,16 +5,26 @@
 #
 # Notes: example from https://byjus.com/maths/determinant-of-4x4-matrix/
 #        +-+-
+#
+#       https://semath.info/src/inverse-cofactor-ex4.html
+#
+#        for inverse of 4x4 which is 1 over the determinant times the adjugate
 #=======================================================================#
 
   # load packages
   library(data.table)
   
   # generate data
-  my_dt <- t(matrix(c(4,  3,  2, 2,
-                      0,  1, -3, 3,
-                      0, -1,  3, 3, 
-                      0,  3,  1, 1),
+  # my_dt <- t(matrix(c(4,  3,  2, 2,
+  #                     0,  1, -3, 3,
+  #                     0, -1,  3, 3, 
+  #                     0,  3,  1, 1),
+  #                   ncol = 4))
+  
+  my_dt <- t(matrix(c(1,  1,  1, -1,
+                      1,  1, -1,  1,
+                      1, -1,  1, 1, 
+                     -1,  1,  1, 1),
                     ncol = 4))
   
   # give dimensions' names
@@ -64,24 +74,43 @@ compute_four_by_four_determinant_terms <- function(cofactor_row, cofactor_column
   # subtract
   wide_dt[, difference := forward_sum - backward_sum]
   
-  # cofactor times difference
-  wide_dt[, determinant_term := cofactor * difference]
+  # include cofactor to compute 4x4 determinant outside of function
+  wide_dt[, cofactor :=  cofactor]
   
   # identify cofactor
-  wide_dt[, cofactor_index := paste(cofactor_row, cofactor_column, sep = "_")]
+  wide_dt[, cofactor_row    := cofactor_row]
+  wide_dt[, cofactor_column := cofactor_column]
   
-  return(wide_dt[, .(cofactor_index, determinant_term)])
+  #note keep the differences for filling in the adjugate
+  return(wide_dt[, .(cofactor_row, cofactor_column, cofactor, difference)])
   
 }
 
   # call function for each cofactor
-  stacked_terms <- rbindlist(lapply(1:nrow(my_dt), function(x) compute_four_by_four_determinant_terms(x, 1)))
+  determinant_stacked_terms <- rbindlist(lapply(1:nrow(my_dt), function(x) compute_four_by_four_determinant_terms(x, 1)))
+  second_adjugate_terms     <- rbindlist(lapply(1:nrow(my_dt), function(x) compute_four_by_four_determinant_terms(x, 2)))
+  third_adjugate_terms      <- rbindlist(lapply(1:nrow(my_dt), function(x) compute_four_by_four_determinant_terms(x, 3)))
+  fourth_adjugate_terms     <- rbindlist(lapply(1:nrow(my_dt), function(x) compute_four_by_four_determinant_terms(x, 4)))
+  
+  # stack adjugate terms
+  stacked_adjugate <- rbind(determinant_stacked_terms, second_adjugate_terms, third_adjugate_terms, fourth_adjugate_terms)
+  
+  # compute cofactor products
+  determinant_stacked_terms[, determinant_term := cofactor * difference]
 
   # cast wide to get the +- pattern right  
-  predeterminant <- dcast.data.table(stacked_terms, ... ~ paste0("cf_", cofactor_index), value.var = "determinant_term")[, -1]
+  wide_determinant <- dcast.data.table(determinant_stacked_terms[, -c("cofactor", "difference")], ... ~ paste0("cf_", cofactor_row, "_", cofactor_column), value.var = "determinant_term")[, -1]
   
   # compute determinant
-  predeterminant[, determinant := cf_1_1 - cf_2_1 + cf_3_1 - cf_4_1]
+  determinant <- wide_determinant[, cf_1_1 - cf_2_1 + cf_3_1 - cf_4_1]
+  
+  # first term of adjugate depends on even/odd dimsums
+  stacked_adjugate[, sign := (-1) ^ (cofactor_row + cofactor_column)]
+  
+  # the inverse is sign times difference times 1 / determinant
+  stacked_adjugate[, inverse_element := 1 / determinant * sign * difference]
+  
+  
   
   
                       
